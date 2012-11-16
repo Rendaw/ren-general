52b3a99e0351ee8bccde49197bfe4468d1b4378a
Rendaw <rendaw@zarbosoft.com>
2012-11-16 19:47:36 +0900

    Fixed native string conversion empty corner case.

--- string.h
+++ string.h
@@ -23,6 +23,7 @@ typedef std::u16string NativeString;
 inline NativeString AsNativeString(String const &Input)
 {
 	static_assert(sizeof(wchar_t) == sizeof(char16_t), "Assumption that all Windows systems use 16-bit wide characters failed!");
+	if (Input.empty()) return NativeString();
 	const int Length = MultiByteToWideChar(CP_UTF8, 0, Input.c_str(), Input.length(), nullptr, 0);
 	assert(Length > 0);
 	std::vector<char16_t> ConversionBuffer;
