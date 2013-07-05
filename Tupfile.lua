147d5ff81622b699617dbf0b9c9329c1c88db9ae
Rendaw <rendaw@zarbosoft.com>
2013-07-06 08:02:19 +0900

    Added build-configured linux data directories.

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,2 +1,16 @@
-GeneralObjects = Define.Objects{ Sources = Item '*.cxx' }
+DoOnce 'info.lua'
+
+local BuildFlags
+if tup.getconfig('PLATFORM') == 'ubuntu12' or
+	tup.getconfig('PLATFORM') == 'ubuntu12_64' or
+	tup.getconfig('PLATFORM') == 'arch64'
+then
+	BuildFlags = '-DRENGENERAL_FILESYSTEM_DATALOCATION=\\"/usr/share/' .. Info.PackageName .. '\\"'
+end
+
+GeneralObjects = Define.Objects
+{ 
+	Sources = Item '*.cxx',
+	BuildFlags = BuildFlags
+}
 
